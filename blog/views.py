8b6e793e89685759678c18991978312faadc179c
MatejBivr <matej.pretnar@protonmail.ch>
2019-08-11 14:09:29 +0200

    initial

--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-  {
-    'author': 'some guy',
-    'title': 'some blog post',
-    'content': 'some content',
-    'date_posted': 'june 2019'
-  },
-  {
-    'author': 'some guy',
-    'title': 'some blog post 2',
-    'content': 'some content 2',
-    'date_posted': 'june 2019'
-  },
-  {
-    'author': 'some guy',
-    'title': 'some blog post 3',
-    'content': 'some content 3',
-    'date_posted': 'june 2019'
-  },
-  {
-    'author': 'some guy',
-    'title': 'some blog post 4',
-    'content': 'some content 4',
-    'date_posted': 'june 2019'
-  }
-]
+from .models import Post
 
 def home(request):
   context = {
-    'posts': posts
+    'posts': Post.objects.all()
   }
   return render(request, 'blog/home.html', context)
 
